25bb8c13b09d9b8057688acc31cf76609fc16535
ваше_имя <a.baraxanov12@gmail.com>
2026-02-20 09:52:33 +0300

    убрано уловие из Hello.pas

--- Hello.pas
+++ Hello.pas
@@ -11,13 +11,7 @@ BEGIN
   THEN
     BEGIN
       Delete(QueryString, 1, PosName + 4);
-      PosName := Pos('&', QueryString);
-      IF PosName > 0 
-      THEN 
-        BEGIN
-          Name := Copy(QueryString, 1, PosName - 1);
-          Delete(QueryString, 1, PosName)
-        END
+      Name := Copy(QueryString, 1, PosName - 1);
     ELSE
       Name := QueryString
   END;
